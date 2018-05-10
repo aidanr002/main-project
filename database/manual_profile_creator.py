17a3a7686d60a1e1de296d6ca14174d88d1ee0f9
Aidan Ryan <aidanr002@gmail.com>
2018-05-10 13:04:06 +1000

    CSS Update
    NB: The logo is property of justdebate.co.uk

--- database/manual_profile_creator.py
+++ database/manual_profile_creator.py
@@ -1,18 +1,37 @@
 from customer import *
 # Uses code from NCSS to generate profile !!TEMPORARY!! #
-fields = []
-fname = input("Enter First Name: ")
-lname = input("Enter Last Name: ")
-email = input("Enter Email: ")
-country = input("Enter Country: ")
-bio = input("Enter Bio: ")
-username = input("Enter Username: ")
-password = input("Enter Password: ")
-fields.append(fname)
-fields.append(lname)
-fields.append(email)
-fields.append(country)
-fields.append(bio)
-fields.append(username)
-fields.append(password)
-user = create_user('users.db', fields)
+user = input("Stress test, super stress test or normal entry ")
+if user == "super stress test":
+    for i in range(10000):
+        fields = ["Name" + str(i), "Description" + str(i), "Email" + str(i), "Country" + str(i), "Bio" + str(i), "Username" + str(i), "Password" + str(i)]
+        print (fields)
+        topic = create_user('users.db', fields)
+if user == "stress test":
+    for i in range(1000):
+        fields = ["Name" + str(i), "Description" + str(i), "Email" + str(i), "Country" + str(i), "Bio" + str(i), "Username" + str(i), "Password" + str(i)]
+        if i == 100 or i == 200 or i == 300 or i == 400 or i == 500 or i == 600 or i == 700 or i ==800 or i == 900:
+            print ("At number " + str(i))
+            user = input("Do you wish to continue? ")
+            if user == 'yes':
+                continue
+            else:
+                break
+        print (fields)
+        topic = create_user('users.db', fields)
+if user == "normal entry":
+    fields = []
+    fname = input("Enter First Name: ")
+    lname = input("Enter Last Name: ")
+    email = input("Enter Email: ")
+    country = input("Enter Country: ")
+    bio = input("Enter Bio: ")
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+    fields.append(fname)
+    fields.append(lname)
+    fields.append(email)
+    fields.append(country)
+    fields.append(bio)
+    fields.append(username)
+    fields.append(password)
+    user = create_user('users.db', fields)
--- todo.md
+++ todo.md
@@ -8,15 +8,19 @@
 - You can't access other peoples profiles (/user/[other persons id]/)
 - Create comments
 - Anonymous profile (in setup)
+- Merge topicpage into one file - not four (only difference is comments [if statement can do the same thing])
 
 """ Yet To Do """
 - Create topics
+- Alert TOU
 - Poll - leave POV without comment (Tally all)
 - Review/remove topics/ comments
 - # HTML & CSS for all pages (actually make it look good)
+    - Topics pages
+    - Search Results pages
+    - Profile pages
 - Edit/remove profile
 - See your comments and remove your comments
-- Merge topicpage into one file - not four (only difference is comments [if statement can do the same thing])
 
 ## Bugs / Problems
 - Redirect after login to last page (not home)
